5cdc04270d34963fe994997f401cb6870df91659
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-02 23:51:08 +0530

    Assignment on Fibonacci Series

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1, a2 = 1, n = 8, a3 = 0;
+            Console.Write(a1 + " ");
+            Console.Write(a2 + " ");
+            for (int i = 2; i < n; i++)
+            {
+                a3 = a1 + a2;
+                Console.Write(a3 + " ");
+                a1 = a2;
+                a2 = a3;
+            }
+        }
+    }
+}
